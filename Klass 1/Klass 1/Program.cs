9dbe2951b47c920edfff50a003f5f65f49be2328
Evgenia18 <118425098+Evgenia18@users.noreply.github.com>
2022-11-22 14:08:31 +0900

    Add files via upload

---
+++ Klass 1/Klass 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Fasz
+{
+    public static void Main()
+    {
+        int p = 0;
+        int vzx = 0;
+        try
+        {
+            vzx = 100 / p;
+            Console.WriteLine("Эта строка не выполнена");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Возникло исключение");
+        }
+        Console.WriteLine($"Результатом является {vzx}");
+    }
+}
